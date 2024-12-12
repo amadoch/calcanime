ddf693a26d1b271ae98f1e32198c9edada36d7d3
Amadoch <abec2003@hotmail.com>
2024-12-12 19:55:01 -0400

    Mejoras con respecto a: - Interfaz de usuario - Enrutamiento archivos estaticos

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 'use strict'
-import * as status from './logger/status.js';
+import wPath from 'node:path/win32'
+import { fileURLToPath } from 'node:url';
+import * as status from './dev/logger/status.js';
 import express from 'express';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = wPath.dirname(__filename);
+
 const port = process.env.port || 1337;
+const app = express();
 
 app.get('/', (req, res) => {
-    status.serverHasResponse(res.url);
-    res.send('Hola Calcanime');
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+//Redirección a las paginas estáticas
+app.use('/static', express.static(__dirname + '/public/static'));
+
+//Fin del enrutamiento.
+app.use((req, res) => {
+    res.send('<h1>Lo sentimos, no hemos podido encontrar la página.</h1>');
 });
 
 app.listen(port, () => {
     status.serverAsStart(port);
 });
-
-status.serverAsClose();
